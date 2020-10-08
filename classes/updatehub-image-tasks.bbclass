UpdateHub/meta-updatehub

commit 2f3b38f1c79c1d4bb4f8cd89ab3e57acaee3f66b

classes/updatehub-image-tasks: Fix build error when using `rm_work`
We need to ensure that `uhu-native:do_populate_sysroot` is complete
before triggering the tasks so we ensure we have all dependencies
satisfied.

This fixes errors when using the system together with `rm_work` class
loaded.

Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

classes/updatehub-image-tasks.bbclass
@@ -24,8 +24,6 @@ def uhupkg_search(files, search_path):
             if searched:
                 return searched
 
-DEPENDS += "uhu-native"
-
 python () {
     ### Queue initramfs image build
     image_type = d.getVar("UPDATEHUB_IMAGE_TYPE", False)
@@ -140,6 +138,7 @@ python do_uhushell () {
 }
 
 addtask uhushell after do_image_complete do_unpack
+do_uhushell[depends] += "uhu-native:do_populate_sysroot"
 do_uhushell[dirs] ?= "${DEPLOY_DIR_IMAGE}"
 do_uhushell[nostamp] = "1"
 
@@ -158,6 +157,7 @@ python do_uhuarchive () {
 }
 
 addtask uhuarchive after do_image_complete do_unpack
+do_uhuarchive[depends] += "uhu-native:do_populate_sysroot"
 do_uhuarchive[nostamp] = "1"
 do_uhuarchive[recrdeptask] += "do_deploy"
 

commit add6262c7d705a8c0e40f6484539cdb973732cd6

classes/updatehub-image-tasks: Add missing dependency on uhu-native
When reworked to fix the build error when using the `rm_work` class, we
end not adding the required dependency to the `uhupush` task.

Reported-by: Bart van der Laan <bartvanderlaan@gmail.com>
Fixes: 2f3b38f "classes/updatehub-image-tasks: Fix build error when using `rm_work`"
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

classes/updatehub-image-tasks.bbclass
@@ -183,6 +183,7 @@ python do_uhupush () {
 }
 
 addtask uhupush after do_image_complete do_unpack
+do_uhupush[depends] += "uhu-native:do_populate_sysroot"
 do_uhupush[nostamp] = "1"
 do_uhupush[recrdeptask] += "do_deploy"
 