UpdateHub/meta-updatehub

commit cda38db130a1ab10239a7d3504da3263c9d3231c

Apply convert-overrides script against the layer
The script applied is a conversion script to add
new override syntax to existing bitbake metadata.

The current revision of the script is 41bff44 and can
be found on oe-core repository.

Signed-off-by: Vinicius Aquino <vinicius.aquino@ossystems.com.br>

recipes-core/packagegroups/packagegroup-updatehub.bb
@@ -5,41 +5,41 @@ LICENSE = "MIT"
 # The package vary from one machine to another
 PACKAGE_ARCH = "${MACHINE_ARCH}"
 
-VIRTUAL-RUNTIME_updatehub-certificates ?= "ca-certificates"
-VIRTUAL-RUNTIME_updatehub-config ?= "updatehub-config"
-VIRTUAL-RUNTIME_updatehub-callbacks ?= "updatehub-callbacks"
-VIRTUAL-RUNTIME_updatehub-system-version ?= "os-release"
-VIRTUAL-RUNTIME_updatehub-system-inquiry ?= "updatehub-system-inquiry"
-VIRTUAL-RUNTIME_updatehub-machine-info ?= "updatehub-machine-info"
+VIRTUAL-RUNTIME:updatehub-certificates ?= "ca-certificates"
+VIRTUAL-RUNTIME:updatehub-config ?= "updatehub-config"
+VIRTUAL-RUNTIME:updatehub-callbacks ?= "updatehub-callbacks"
+VIRTUAL-RUNTIME:updatehub-system-version ?= "os-release"
+VIRTUAL-RUNTIME:updatehub-system-inquiry ?= "updatehub-system-inquiry"
+VIRTUAL-RUNTIME:updatehub-machine-info ?= "updatehub-machine-info"
 
 inherit packagegroup updatehub-runtime
 
 PACKAGES += "${PN}-initramfs-runtime ${PN}-initramfs-support ${PN}-active-inactive-runtime"
 
-RDEPENDS_${PN}-active-inactive-runtime += " \
-    ${VIRTUAL-RUNTIME_updatehub-certificates} \
-    ${VIRTUAL-RUNTIME_updatehub-config} \
-    ${VIRTUAL-RUNTIME_updatehub-callbacks} \
-    ${VIRTUAL-RUNTIME_updatehub-machine-info} \
-    ${VIRTUAL-RUNTIME_updatehub-system-version} \
-    ${VIRTUAL-RUNTIME_updatehub-system-inquiry} \
+RDEPENDS:${PN}-active-inactive-runtime += " \
+    ${VIRTUAL-RUNTIME:updatehub-certificates} \
+    ${VIRTUAL-RUNTIME:updatehub-config} \
+    ${VIRTUAL-RUNTIME:updatehub-callbacks} \
+    ${VIRTUAL-RUNTIME:updatehub-machine-info} \
+    ${VIRTUAL-RUNTIME:updatehub-system-version} \
+    ${VIRTUAL-RUNTIME:updatehub-system-inquiry} \
     ${UPDATEHUB_RUNTIME_PACKAGES} \
     updatehub \
 "
 
-RDEPENDS_${PN}-initramfs-runtime += " \
-    ${VIRTUAL-RUNTIME_updatehub-config} \
-    ${VIRTUAL-RUNTIME_updatehub-callbacks} \
-    ${VIRTUAL-RUNTIME_updatehub-machine-info} \
-    ${VIRTUAL-RUNTIME_updatehub-system-inquiry} \
+RDEPENDS:${PN}-initramfs-runtime += " \
+    ${VIRTUAL-RUNTIME:updatehub-config} \
+    ${VIRTUAL-RUNTIME:updatehub-callbacks} \
+    ${VIRTUAL-RUNTIME:updatehub-machine-info} \
+    ${VIRTUAL-RUNTIME:updatehub-system-inquiry} \
     ${UPDATEHUB_RUNTIME_PACKAGES} \
 "
 
-RDEPENDS_${PN}-initramfs-support += " \
-    ${VIRTUAL-RUNTIME_updatehub-certificates} \
-    ${VIRTUAL-RUNTIME_updatehub-config} \
-    ${VIRTUAL-RUNTIME_updatehub-callbacks} \
-    ${VIRTUAL-RUNTIME_updatehub-machine-info} \
-    ${VIRTUAL-RUNTIME_updatehub-system-inquiry} \
+RDEPENDS:${PN}-initramfs-support += " \
+    ${VIRTUAL-RUNTIME:updatehub-certificates} \
+    ${VIRTUAL-RUNTIME:updatehub-config} \
+    ${VIRTUAL-RUNTIME:updatehub-callbacks} \
+    ${VIRTUAL-RUNTIME:updatehub-machine-info} \
+    ${VIRTUAL-RUNTIME:updatehub-system-inquiry} \
     updatehub \
 "

classes/updatehub-runtime.bbclass
@@ -53,7 +53,7 @@ UPDATEHUB_COMPATIBLE_MACHINE ?= "${MACHINE}"
 # This happens because the LC_ALL variable is not set to use UTF-8, e.g. en_US.UTF-8 and libarchive
 # can't extract files uses UTF-8 format, e.g. ca-certificate NetLock_Arany_=Class_Gold=_Főtanúsítvány.pem
 # file.
-IMAGE_CMD_tar = "${IMAGE_CMD_TAR} --sort=name --format=gnu --numeric-owner -cf ${IMGDEPLOYDIR}/${IMAGE_NAME}${IMAGE_NAME_SUFFIX}.tar -C ${IMAGE_ROOTFS} . || [ $? -eq 1 ]"
+IMAGE_CMD:tar = "${IMAGE_CMD_TAR} --sort=name --format=gnu --numeric-owner -cf ${IMGDEPLOYDIR}/${IMAGE_NAME}${IMAGE_NAME_SUFFIX}.tar -C ${IMAGE_ROOTFS} . || [ $? -eq 1 ]"
 
 python () {
     if bb.data.inherits_class('image', d):

conf/layer.conf
@@ -7,7 +7,7 @@ BBFILES += "${LAYERDIR}/recipes-*/*/*.bb \
 
 BBFILE_COLLECTIONS += "updatehub"
 BBFILE_PATTERN_updatehub = "^${LAYERDIR}/"
-BBFILE_PRIORITY_updatehub = "8"
+BBFILE_PRIORITY:updatehub = "8"
 
 SIGGEN_EXCLUDERECIPES_ABISAFE += " \
     updatehub-machine-info \
@@ -15,7 +15,7 @@ SIGGEN_EXCLUDERECIPES_ABISAFE += " \
 
 LAYERSERIES_COMPAT_updatehub = "honister"
 
-LAYERDEPENDS_updatehub = " \
+LAYERDEPENDS:updatehub = " \
     core \
     meta-python \
     networking-layer \
@@ -32,6 +32,6 @@ BBFILES_DYNAMIC += " \
 UPDATEHUB_PRODUCT_UID ??= "0000000000000000000000000000000000000000000000000000000000000000"
 
 UPDATEHUB_PACKAGE_VERSION_SUFFIX ??= ""
-UPDATEHUB_PACKAGE_VERSION_append = "${UPDATEHUB_PACKAGE_VERSION_SUFFIX}"
+UPDATEHUB_PACKAGE_VERSION:append = "${UPDATEHUB_PACKAGE_VERSION_SUFFIX}"
 
 PREFERRED_RPROVIDER_virtual/grub-bootconf ??= "updatehub-grub-script"

dynamic-layers/qt5/recipes-updatehub/updatehub-sdk-qt/updatehub-sdk-qt_git.bb
@@ -19,10 +19,10 @@ EXTRA_QMAKEVARS_PRE += " \
   ${PACKAGECONFIG_CONFARGS} \
 "
 
-RDEPENDS_${PN} += "updatehub-sdk-statechange-trigger"
+RDEPENDS:${PN} += "updatehub-sdk-statechange-trigger"
 
 PACKAGES += "${PN}-qmlplugin"
 
-FILES_${PN}-qmlplugin += "${OE_QMAKE_PATH_QML}/updatehub"
-RDEPENDS_${PN}-qmlplugin += "qtdeclarative-qmlplugins"
-INSANE_SKIP_${PN}-qmlplugin = "dev-so"
+FILES:${PN}-qmlplugin += "${OE_QMAKE_PATH_QML}/updatehub"
+RDEPENDS:${PN}-qmlplugin += "qtdeclarative-qmlplugins"
+INSANE_SKIP:${PN}-qmlplugin = "dev-so"

recipes-bsp/grub/updatehub-grub-script.bb
@@ -20,8 +20,8 @@ do_configure[noexec] = "1"
 do_compile[noexec] = "1"
 do_install[noexec] = "1"
 
-RPROVIDES_${PN} += "virtual/grub-bootconf"
+RPROVIDES:${PN} += "virtual/grub-bootconf"
 
-ALLOW_EMPTY_${PN} = "1"
+ALLOW_EMPTY:${PN} = "1"
 
 PACKAGE_ARCH = "${MACHINE_ARCH}"

recipes-bsp/u-boot/u-boot-updatehub-script.bb
@@ -174,9 +174,9 @@ do_deploy() {
 
 addtask deploy after do_install before do_build
 
-FILES_${PN} += "/"
+FILES:${PN} += "/"
 
 PROVIDES += "u-boot-default-script"
-RPROVIDES_${PN} += "u-boot-default-script"
+RPROVIDES:${PN} += "u-boot-default-script"
 
 PACKAGE_ARCH = "${MACHINE_ARCH}"

recipes-bsp/updatehub-active-inactive-backend/updatehub-active-inactive-backend-grub-efi.bb
@@ -13,6 +13,6 @@ do_install () {
     install -Dm 0644 updatehub-grubenv.default ${D}${sysconfdir}/default/updatehub-grubenv
 }
 
-RDEPENDS_${PN} += "grub-efi-editenv updatehub-active-inactive-backend-grub-tools"
+RDEPENDS:${PN} += "grub-efi-editenv updatehub-active-inactive-backend-grub-tools"
 
 PACKAGE_ARCH = "${MACHINE_ARCH}"

recipes-bsp/updatehub-active-inactive-backend/updatehub-active-inactive-backend-grub.bb
@@ -13,6 +13,6 @@ do_install () {
     install -Dm 0644 updatehub-grubenv.default ${D}${sysconfdir}/default/updatehub-grubenv
 }
 
-RDEPENDS_${PN} += "grub-editenv updatehub-active-inactive-backend-grub-tools"
+RDEPENDS:${PN} += "grub-editenv updatehub-active-inactive-backend-grub-tools"
 
 PACKAGE_ARCH = "${MACHINE_ARCH}"

commit fa1bf68fc6c567cf15911bd8fc5f99ca9337745d

packagegroup-updatehub.bb: Fix variables syntax
This commit reverts wrong modifications caused by commit cda38db1

Signed-off-by: Abner C. Paula <abner.cordeiro@ossystems.com.br>

recipes-core/packagegroups/packagegroup-updatehub.bb
@@ -5,41 +5,41 @@ LICENSE = "MIT"
 # The package vary from one machine to another
 PACKAGE_ARCH = "${MACHINE_ARCH}"
 
-VIRTUAL-RUNTIME:updatehub-certificates ?= "ca-certificates"
-VIRTUAL-RUNTIME:updatehub-config ?= "updatehub-config"
-VIRTUAL-RUNTIME:updatehub-callbacks ?= "updatehub-callbacks"
-VIRTUAL-RUNTIME:updatehub-system-version ?= "os-release"
-VIRTUAL-RUNTIME:updatehub-system-inquiry ?= "updatehub-system-inquiry"
-VIRTUAL-RUNTIME:updatehub-machine-info ?= "updatehub-machine-info"
+VIRTUAL-RUNTIME_updatehub-certificates ?= "ca-certificates"
+VIRTUAL-RUNTIME_updatehub-config ?= "updatehub-config"
+VIRTUAL-RUNTIME_updatehub-callbacks ?= "updatehub-callbacks"
+VIRTUAL-RUNTIME_updatehub-system-version ?= "os-release"
+VIRTUAL-RUNTIME_updatehub-system-inquiry ?= "updatehub-system-inquiry"
+VIRTUAL-RUNTIME_updatehub-machine-info ?= "updatehub-machine-info"
 
 inherit packagegroup updatehub-runtime
 
 PACKAGES += "${PN}-initramfs-runtime ${PN}-initramfs-support ${PN}-active-inactive-runtime"
 
 RDEPENDS:${PN}-active-inactive-runtime += " \
-    ${VIRTUAL-RUNTIME:updatehub-certificates} \
-    ${VIRTUAL-RUNTIME:updatehub-config} \
-    ${VIRTUAL-RUNTIME:updatehub-callbacks} \
-    ${VIRTUAL-RUNTIME:updatehub-machine-info} \
-    ${VIRTUAL-RUNTIME:updatehub-system-version} \
-    ${VIRTUAL-RUNTIME:updatehub-system-inquiry} \
+    ${VIRTUAL-RUNTIME_updatehub-certificates} \
+    ${VIRTUAL-RUNTIME_updatehub-config} \
+    ${VIRTUAL-RUNTIME_updatehub-callbacks} \
+    ${VIRTUAL-RUNTIME_updatehub-machine-info} \
+    ${VIRTUAL-RUNTIME_updatehub-system-version} \
+    ${VIRTUAL-RUNTIME_updatehub-system-inquiry} \
     ${UPDATEHUB_RUNTIME_PACKAGES} \
     updatehub \
 "
 
 RDEPENDS:${PN}-initramfs-runtime += " \
-    ${VIRTUAL-RUNTIME:updatehub-config} \
-    ${VIRTUAL-RUNTIME:updatehub-callbacks} \
-    ${VIRTUAL-RUNTIME:updatehub-machine-info} \
-    ${VIRTUAL-RUNTIME:updatehub-system-inquiry} \
+    ${VIRTUAL-RUNTIME_updatehub-config} \
+    ${VIRTUAL-RUNTIME_updatehub-callbacks} \
+    ${VIRTUAL-RUNTIME_updatehub-machine-info} \
+    ${VIRTUAL-RUNTIME_updatehub-system-inquiry} \
     ${UPDATEHUB_RUNTIME_PACKAGES} \
 "
 
 RDEPENDS:${PN}-initramfs-support += " \
-    ${VIRTUAL-RUNTIME:updatehub-certificates} \
-    ${VIRTUAL-RUNTIME:updatehub-config} \
-    ${VIRTUAL-RUNTIME:updatehub-callbacks} \
-    ${VIRTUAL-RUNTIME:updatehub-machine-info} \
-    ${VIRTUAL-RUNTIME:updatehub-system-inquiry} \
+    ${VIRTUAL-RUNTIME_updatehub-certificates} \
+    ${VIRTUAL-RUNTIME_updatehub-config} \
+    ${VIRTUAL-RUNTIME_updatehub-callbacks} \
+    ${VIRTUAL-RUNTIME_updatehub-machine-info} \
+    ${VIRTUAL-RUNTIME_updatehub-system-inquiry} \
     updatehub \
 "